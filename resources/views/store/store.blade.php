36be50ec3500741366cf62efda1d2450f60eca67
Rodrigo Falco <rodrigo@starcominformatica.com>
2015-10-09 21:07:31 -0300

    ELIXIR ASSETS

--- resources/views/store/store.blade.php
+++ resources/views/store/store.blade.php
@@ -8,12 +8,7 @@
     <title>Home | E-Shop</title>
 
 
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/font-awesome.min.css" rel="stylesheet">
-    <link href="/css/prettyPhoto.css" rel="stylesheet">
-    <link href="/css/animate.css" rel="stylesheet">
-    <link href="/css/main.css" rel="stylesheet">
-    <link href="/css/responsive.css" rel="stylesheet">
+    <link href="{{ elixir('css/all.css') }}" rel="stylesheet">
 
 
 </head><!--/head-->
@@ -136,7 +131,7 @@
     <div class="footer-bottom">
         <div class="container">
             <div class="row">
-                <p class="pull-left">Copyright © 2013 E-Shop Inc. All rights reserved.</p>
+                <p class="pull-left">Copyright Â© 2013 E-Shop Inc. All rights reserved.</p>
                 <p class="pull-right">Designed by <span><a target="_blank" href="http://invoinn.com/">InvoInn</a></span></p>
             </div>
         </div>
@@ -146,11 +141,7 @@
 
 
 
-<script src="/js/jquery.js"></script>
-<script src="/js/bootstrap.min.js"></script>
-<script src="/js/jquery.scrollUp.min.js"></script>
-<script src="/js/jquery.prettyPhoto.js"></script>
-<script src="/js/main.js"></script>
+<script src="{{ elixir('js/all.js') }}"></script>
 
 </body>
 </html>
